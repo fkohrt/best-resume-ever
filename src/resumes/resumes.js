49816231bae4c6126b6596ea3ff02720d9d0bafb
Florian Kohrt <fkohrt@anche.no>
2020-04-27 20:41:09 +0200

    Remove other resumes

--- src/resumes/resumes.js
+++ src/resumes/resumes.js
@@ -1,12 +1,12 @@
 // If you create a new resume, import it here:
-import './material-dark.vue';
-import './material-dark-persona-v1.vue';
+/*import './material-dark.vue';
+import './material-dark-persona-v1.vue';*/
 import './material-dark-persona-v2.vue';
-import './left-right.vue';
+/*import './left-right.vue';
 import './oblique.vue';
 import './side-bar.vue';
 import './purple.vue';
 import './side-bar-rtl.vue';
 import './left-right-rtl.vue';
 import './creative.vue';
-import './cool.vue';
+import './cool.vue';*/
